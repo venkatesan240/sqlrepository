5921c65d36f1e12bff7fcbfd1ee64b125ce03ea9
venkatesan <163666111+venkatesan240@users.noreply.github.com>
2024-04-18 17:31:27 +0530

    Delete hotal.sql

--- hotal.sql
+++
@@ -1,7 +0,0 @@
-create table bank;
-use bank;
-create table details(account_number int(10)PRIMARY KEY,username varchar(20) not null,ifsccode varchar(20),branch varchar(20),phoneNumber long,account int(10));
-desc details;
-insert into details(account_number,username,ifsccode,branch,phonenumber,account)values(104,"gokul","CGGI010","sivakasi",7865349856,4000);
-select * from details;
-delete from details where account_number=104;
